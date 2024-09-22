c2c4779358660ef088e8744f0273354a886b1a10
Rully Agus Hendrawan <ruhendrawan@pitt.edu>
2024-09-22 17:10:29 -0400

    Demo mac shortcuts and vim

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter } from "@angular/router";
 
+import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 
+import { provideHttpClient } from "@angular/common/http";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideHttpClientTesting(),
+  ],
 };
--- src/app/cheatsheet/cheatsheet.component.ts
+++ src/app/cheatsheet/cheatsheet.component.ts
@@ -2,8 +2,9 @@ import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { Component } from "@angular/core";
 
 import { CommonModule } from "@angular/common";
-import { Keymap } from "../../utils/keymaps";
 import { FormsModule } from "@angular/forms";
+import { Keymap } from "../../utils/keymaps";
+import { empty } from "rxjs";
 
 @Component({
   selector: "app-cheatsheet",
@@ -14,6 +15,9 @@ import { FormsModule } from "@angular/forms";
 })
 export class CheatsheetComponent {
   list: Keymap[] = [];
+  listGrouped: { [key: string]: Keymap[] } = {};
+  listContexts: string[] = [];
+  chunkContexts: string[][] = [];
   listFiltered: Keymap[] = [];
 
   textFilter = "";
@@ -24,13 +28,105 @@ export class CheatsheetComponent {
     this.http.get<Keymap[]>("keymaps/vim.json").subscribe((data) => {
       this.list = data;
       this.listFiltered = data;
+      this.updateListGrouped(data);
     });
+  }
+
+  updateListGrouped(keymap: Keymap[]): void {
+    this.listGrouped = keymap.reduce((groups, km) => {
+      if (!groups[km.context]) {
+        groups[km.context] = [];
+      }
+      km.motion = this.reorganizeModifiers(km.motion);
+      groups[km.context].push(km);
+      return groups;
+    }, {});
+
+    this.listContexts = Object.keys(this.listGrouped).sort();
+
+    let colMax = 2;
+    let chunkSize = Math.ceil(this.listContexts.length / colMax);
+    console.log(chunkSize);
+    this.chunkContexts = this.listContexts.reduce((acc, context, index) => {
+      if (index % chunkSize === 0) {
+        acc.push(this.listContexts.slice(index, index + chunkSize));
+      }
+      return acc;
+    }, []);
+  }
+
+  reorganizeModifiers(motion: string): string {
+    let motions = motion.split(" ");
+
+    for (let m = 0; m < motions.length; m++) {
+      let hotkeys = motions[m].split("+");
+
+      let newkeys = "";
+      let is_alt = false;
+      let is_ctrl = false;
+      let is_shift = false;
+      let is_cmd = false;
+      for (let i = 0; i < hotkeys.length; i++) {
+        let hotkey = hotkeys[i].toLowerCase();
+        if (["alt", "option", "opt"].includes(hotkey)) {
+          hotkeys[i] = "";
+          is_alt = true;
+        } else if (
+          ["cmd", "command", "win", "meta", "winkey", "windows"].includes(
+            hotkey,
+          )
+        ) {
+          is_cmd = true;
+          hotkeys[i] = "";
+        } else if (["ctrl", "control"].includes(hotkey)) {
+          is_ctrl = true;
+          hotkeys[i] = "";
+        } else if (["shift"].includes(hotkey)) {
+          is_shift = true;
+          hotkeys[i] = "";
+        }
+      }
+
+      if (is_ctrl) {
+        newkeys += "C-";
+      }
+      if (is_alt) {
+        newkeys += "A-";
+      }
+      if (is_shift) {
+        newkeys += "S-";
+      }
+      if (is_cmd) {
+        newkeys += "M-";
+      }
+
+      for (let i = 0; i < hotkeys.length; i++) {
+        if (hotkeys[i]) {
+          newkeys += hotkeys[i];
+        }
+      }
+
+      motions[m] = newkeys;
+    }
+
+    return motions.join(" ");
   }
 
   onFilterChange(): void {
     const filter = this.textFilter.toLowerCase();
-    this.listFiltered = this.list.filter((key) =>
-      key.motion.toLowerCase().includes(filter),
-    );
+
+    // Split the filter text into tokens for more flexible matching
+    const tokens = filter.split(" ").filter((token) => token);
+
+    this.listFiltered = this.list.filter((keymap) => {
+      const motion = keymap.motion.toLowerCase();
+      const description = keymap.description.toLowerCase();
+
+      return tokens.every(
+        (token) => motion.includes(token) || description.includes(token),
+      );
+    });
+
+    this.updateListGrouped(this.listFiltered);
   }
 }
