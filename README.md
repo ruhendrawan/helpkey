220905bdd4a5edd2a2044a8fb03a86cb54d00311
Rully Agus Hendrawan <ruhendrawan@pitt.edu>
2024-09-21 20:47:57 -0400

    enable hotkeys, non strict ts

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Tauri + Angular
+# HelpKey
 
-This template should help get you started developing with Tauri and Angular.
-
-## Recommended IDE Setup
-
-[VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer) + [Angular Language Service](https://marketplace.visualstudio.com/items?itemName=Angular.ng-template).
+Help me remember which keys I need to press.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,75 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, ChangeDetectorRef } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+
 import { invoke } from "@tauri-apps/api/tauri";
+import { listen } from "@tauri-apps/api/event";
+import { register } from "@tauri-apps/api/globalShortcut";
+import { appWindow } from "@tauri-apps/api/window";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  greetingMessage = "";
+  activeWindowTitle = "";
+  appOnTop = true;
 
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
+  updateWindowTitle(title: string) {
+    this.activeWindowTitle = title;
+  }
 
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
+  async fetchActiveWindowTitle() {
+    try {
+      const title = await invoke("get_active_window_info");
+      console.log(`Active window/tab title: ${title}`);
+      this.updateWindowTitle(`${title}`);
+      // Angular change detection is not triggered when the window is focused programmatically
+      this.cdRef.detectChanges();
+    } catch (error) {
+      console.error("Error fetching active window/tab title:", error);
+      if (error.includes("Accessibility permissions are not enabled")) {
+        // Show an HTML page or a modal with instructions
+        this.showPermissionInstructions();
+      }
+    }
+  }
+
+  showPermissionInstructions() {
+    // You can use your frontend framework to display a modal or navigate to a page
+    // For simplicity, let's open a new window with the instructions
+    window.open(
+      "https://osxdaily.com/2014/01/05/enable-access-assistive-devices-apps-mac-os-x/",
+      "_blank",
+    );
+  }
+
+  registerGlobalShortcut() {
+    register("Control+Shift+Z", () => {
+      console.log("Shortcut triggered");
+      if (this.appOnTop) {
+        appWindow.hide();
+        appWindow.setAlwaysOnTop(false);
+        this.appOnTop = false;
+        return;
+      } else {
+        this.fetchActiveWindowTitle();
+        appWindow.show();
+        appWindow.setAlwaysOnTop(true);
+        appWindow.setFocus();
+        this.appOnTop = true;
+      }
     });
+  }
+
+  constructor(private cdRef: ChangeDetectorRef) {
+    this.registerGlobalShortcut();
+    this.fetchActiveWindowTitle().then(() => {});
+    // listen("refresh-active-window-title", () => {
+    //   this.fetchActiveWindowTitle();
+    // });
   }
 }
